b475deba4e186667ee4736c25ab6e40ea0497565
ya <ya@dei.su>
2019-01-17 15:42:09 +0300

    Space -> Tab

--- __init__.py
+++ __init__.py
@@ -55,129 +55,127 @@ logging.getLogger('').addHandler(console)
 
 
 class PyWavesException(ValueError):
-    pass
+	pass
 
 
 def throw_error(msg):
-    if THROW_EXCEPTION_ON_ERROR:
-        raise PyWavesException(msg)
+	if THROW_EXCEPTION_ON_ERROR:
+		raise PyWavesException(msg)
 
 
 def setThrowOnError(throw=True):
-    global THROW_EXCEPTION_ON_ERROR
-    THROW_EXCEPTION_ON_ERROR = throw
+	global THROW_EXCEPTION_ON_ERROR
+	THROW_EXCEPTION_ON_ERROR = throw
 
 
 def setOffline():
-    global OFFLINE
-    OFFLINE = True
+	global OFFLINE
+	OFFLINE = True
 
 def setOnline():
-    global OFFLINE
-    OFFLINE = False
+	global OFFLINE
+	OFFLINE = False
 
 def setChain(chain = CHAIN, chain_id = None):
-    global CHAIN, CHAIN_ID
+	global CHAIN, CHAIN_ID
 
-    if chain_id is not None:
-        CHAIN = chain
-        CHAIN_ID = chain_id
-    else:
-        if chain.lower()=='mainnet' or chain.lower()=='w':
-            CHAIN = 'mainnet'
-            CHAIN_ID = 'W'
-        elif chain.lower()=='hacknet' or chain.lower()=='u':
-            CHAIN = 'hacknet'
-            CHAIN_ID = 'U'
-        else:
-            CHAIN = 'testnet'
-            CHAIN_ID = 'T'
+	if chain_id is not None:
+		CHAIN = chain
+		CHAIN_ID = chain_id
+	else:
+		if chain.lower()=='mainnet' or chain.lower()=='w':
+			CHAIN = 'mainnet'
+			CHAIN_ID = 'W'
+		elif chain.lower()=='hacknet' or chain.lower()=='u':
+			CHAIN = 'hacknet'
+			CHAIN_ID = 'U'
+		else:
+			CHAIN = 'testnet'
+			CHAIN_ID = 'T'
 
 def getChain():
-    return CHAIN
+	return CHAIN
 
 def setNode(node = NODE, chain = CHAIN, chain_id = None):
-    global NODE, CHAIN, CHAIN_ID
-    NODE = node
-    setChain(chain, chain_id)
+	global NODE, CHAIN, CHAIN_ID
+	NODE = node
+	setChain(chain, chain_id)
 
 def getNode():
-    return NODE
+	return NODE
 
 def setMatcher(node = MATCHER):
-    global MATCHER, MATCHER_PUBLICKEY
-    try:
-        MATCHER_PUBLICKEY = wrapper('/matcher', host = node)
-        MATCHER = node
-        logging.info('Setting matcher %s %s' % (MATCHER, MATCHER_PUBLICKEY))
-    except:
-        MATCHER_PUBLICKEY = ''
+	global MATCHER, MATCHER_PUBLICKEY
+	try:
+		MATCHER_PUBLICKEY = wrapper('/matcher', host = node)
+		MATCHER = node
+		logging.info('Setting matcher %s %s' % (MATCHER, MATCHER_PUBLICKEY))
+	except:
+		MATCHER_PUBLICKEY = ''
 
 def setDatafeed(wdf = DATAFEED):
-    global DATAFEED
-    DATAFEED = wdf
-    logging.info('Setting datafeed %s ' % (DATAFEED))
+	global DATAFEED
+	DATAFEED = wdf
+	logging.info('Setting datafeed %s ' % (DATAFEED))
 
 def wrapper(api, postData='', host='', headers=''):
-    global OFFLINE
-    if OFFLINE:
-        offlineTx = {}
-        offlineTx['api-type'] = 'POST' if postData else 'GET'
-        offlineTx['api-endpoint'] = api
-        offlineTx['api-data'] = postData
-        return offlineTx
-    if not host:
-        host = NODE
-    if postData:
-        req = requests.post('%s%s' % (host, api), data=postData, headers={'content-type': 'application/json'}).json()
-    else:
-        req = requests.get('%s%s' % (host, api), headers=headers).json()
-    return req
+	global OFFLINE
+	if OFFLINE:
+		offlineTx = {}
+		offlineTx['api-type'] = 'POST' if postData else 'GET'
+		offlineTx['api-endpoint'] = api
+		offlineTx['api-data'] = postData
+		return offlineTx
+	if not host:
+		host = NODE
+	if postData:
+		req = requests.post('%s%s' % (host, api), data=postData, headers={'content-type': 'application/json'}).json()
+	else:
+		req = requests.get('%s%s' % (host, api), headers=headers).json()
+	return req
 
 def height():
-    return wrapper('/blocks/height')['height']
+	return wrapper('/blocks/height')['height']
 
 def lastblock():
-    return wrapper('/blocks/last')
+	return wrapper('/blocks/last')
 
 def block(n):
-    return wrapper('/blocks/at/%d' % n)
+	return wrapper('/blocks/at/%d' % n)
 
 def tx(id):
-    return wrapper('/transactions/info/%s' % id)
+	return wrapper('/transactions/info/%s' % id)
 
 def getOrderBook(assetPair):
-    orderBook = assetPair.orderbook()
-    try:
-        bids = orderBook['bids']
-        asks = orderBook['asks']
-    except:
-        bids = ''
-        asks = ''
-    return bids, asks
+	orderBook = assetPair.orderbook()
+	try:
+		bids = orderBook['bids']
+		asks = orderBook['asks']
+	except:
+		bids = ''
+		asks = ''
+	return bids, asks
 
 def symbols():
-    return wrapper('/api/symbols', host=DATAFEED)
+	return wrapper('/api/symbols', host=DATAFEED)
 
 def markets():
-    return wrapper('/api/markets', host=DATAFEED)
+	return wrapper('/api/markets', host=DATAFEED)
 
 def validateAddress(address):
-    addr = crypto.bytes2str(base58.b58decode(address))
-    if addr[0] != chr(ADDRESS_VERSION):
-        logging.error("Wrong address version")
-    elif addr[1] != CHAIN_ID:
-        logging.error("Wrong chain id")
-    elif len(addr) != ADDRESS_LENGTH:
-        logging.error("Wrong address length")
-    elif addr[-ADDRESS_CHECKSUM_LENGTH:] != crypto.hashChain(crypto.str2bytes(addr[:-ADDRESS_CHECKSUM_LENGTH]))[:ADDRESS_CHECKSUM_LENGTH]:
-        logging.error("Wrong address checksum")
-    else:
-        return True
-    return False
+	addr = crypto.bytes2str(base58.b58decode(address))
+	if addr[0] != chr(ADDRESS_VERSION):
+		logging.error("Wrong address version")
+	elif addr[1] != CHAIN_ID:
+		logging.error("Wrong chain id")
+	elif len(addr) != ADDRESS_LENGTH:
+		logging.error("Wrong address length")
+	elif addr[-ADDRESS_CHECKSUM_LENGTH:] != crypto.hashChain(crypto.str2bytes(addr[:-ADDRESS_CHECKSUM_LENGTH]))[:ADDRESS_CHECKSUM_LENGTH]:
+		logging.error("Wrong address checksum")
+	else:
+		return True
+	return False
 
 WAVES = Asset('')
 BTC = Asset('8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS')
 USD = Asset('Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck')
-
-
